c61ef907bf36091077d22b11a1d376517cd8cc14
CescFT <vallcferre@gmail.com>
2026-07-04 11:38:00 +0200

    Raise exception if cp is not valid

--- mercadona-scraper/validations/validate_postal_code.py
+++ mercadona-scraper/validations/validate_postal_code.py
@@ -6,11 +6,11 @@ CONSTANTS_NAME = [
         "VALLS_DATA"
     ]
 
-def validate(postal_code: str) -> dict|None:
+def validate(postal_code: str) -> dict:
 
     for constant in CONSTANTS_NAME:
         city_data = constants_variables_getter(constant)
         if city_data['POSTAL_CODE'] == postal_code:
             return city_data
 
-    return None
+    raise Exception("Postal code info not found")
